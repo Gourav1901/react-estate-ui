49330905991314969ade434c1e95bbbdb321c84c
Gourav1901 <gouravchoudhary2211@gmail.com>
2024-09-25 12:43:13 +0530

    update api

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -2,26 +2,19 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import './map.scss';
 import "leaflet/dist/leaflet.css";
 import Pin from '../pin/pin.jsx';
-function Map({items}){
-  return (
-       <MapContainer center={[52.4797, -1.90269]} zoom={7} scrollWheelZoom={false} className='map'>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
 
-    {/* <Marker position={[52.4797, -1.90269]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker> */
-    
-       items.map(item=>(
-        <Pin item = {item} key={item.id}/>
-       ))
-    }
-  </MapContainer>
-  )
+function Map({ items }) {
+  return (
+    <MapContainer center={[22.7196, 75.8577]} zoom={12} scrollWheelZoom={false} className='map'>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {items.map(item => (
+        <Pin item={item} key={item.id} />
+      ))}
+    </MapContainer>
+  );
 }
 
-export default Map
+export default Map;
